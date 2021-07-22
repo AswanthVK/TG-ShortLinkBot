28490335365506c98943670c5228e8e9f71691c0
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2021-07-22 22:48:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,7 +86,7 @@ async def link_handler(_, update):
     #markup = InlineKeyboardMarkup([[InlineKeyboardButton("Link 🔗", url=shortened_url)]])
     # i don't think this bot with get sending message error so no need of exceptions
     await log_msg.reply_text(text=f"**User Name :** [{update.from_user.first_name}](tg://user?id={update.from_user.id})\n\n**User Id :** `{update.from_user.id}`\n\n**Shortened Link :** {shortened_url}", disable_web_page_preview=True, parse_mode="Markdown", quote=True)
-    await update.reply_text(text=message, reply_markup=markup, quote=True)
+    await update.reply_text(text=message, quote=True)
       
 def get_shortlink(url):
     shortened_url = None
